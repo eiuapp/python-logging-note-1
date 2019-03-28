d59a4e76db3520e0e841f0fc10024edd3368a8ee
tomtsang <tomtsang@mac.pro>
2019-03-28 11:46:40 +0800

    first commit

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# Ceph Handbook
+https://www.cnblogs.com/dahu-daqing/p/7040764.html
 
-此书是[ceph](http://docs.ceph.com)的阅读与理解，帮助大家更快了解ceph的管理和使用。
-
-- GitHub地址：https://github.com/eiuapp/ceph-handbook
-
-- 在线访问地址：https://eiu.app/ceph-handbook
-
-ceph是著名的存储系统。
-
-
-下图是ceph生态圈图：
-
-<!-- https://docs.openstack.org/swift/queens/associated_projects.html -->
-
-<!-- ![ceph生态](images/container-ecosystem.png) -->
-
-## 相关资源
-
-
-## 关于
-
-本书中引用了一些公开的分享与链接并加以整理。
-
-**本书作于2019年初，会持续更新。**
-
-
+ecwid - 体外受精-胚胎移植
---
+++ src/2.4.1.py
@@ -0,0 +1,25 @@
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(level = logging.INFO)
+handler = logging.FileHandler("log.txt")
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+
+logger.addHandler(handler)
+logger.addHandler(console)
+
+logger.info("Start print log")
+logger.debug("Do something")
+logger.warning("Something maybe fail.")
+try:
+    open("sklearn.txt","rb")
+except (SystemExit,KeyboardInterrupt):
+    raise
+except Exception:
+    logger.error("Faild to open sklearn.txt from logger.error",exc_info = True)
+
+logger.info("Finish")
